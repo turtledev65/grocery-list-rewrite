246cc1f93489d227c72f3410c805ef2811128d0a
turtledev <turtledev65@gmail.com>
2024-07-07 20:46:19 +0300

    [frontend] refactor: move socket related types to the socket.ts file

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,25 @@
 "use client";
+
 import { io, Socket } from "socket.io-client";
-import { ClientToServerEvents, ServerToClientEvents } from "./types";
+import { Item, List } from "./types";
+
+type ErrorReponse = { error: string };
+type RespondFunction<T> = T extends undefined
+  ? (val?: ErrorReponse) => void
+  : (val: T | ErrorReponse) => void;
+type EventWithAwk<T extends Record<string, unknown>, R = undefined> = (
+  args: T,
+  respond: RespondFunction<R>,
+) => void;
+
+interface ServerToClientEvents {}
+interface ClientToServerEvents {
+  "create-list": EventWithAwk<{ name: string }, List>;
+  "delete-list": EventWithAwk<{ id: string }>;
+  "join-list": EventWithAwk<{ id: string }>;
+  "get-list": EventWithAwk<{ id: string }, List>;
+  "add-item": EventWithAwk<{ listId: string; text: string }, Item>;
+}
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
   "http://localhost:4000",
--- client/src/types.d.ts
+++ client/src/types.d.ts
@@ -16,22 +16,3 @@ export type Image = {
   itemId: string;
   url: string;
 };
-
-// Socket Events
-export type ErrorReponse = { error: string };
-export type RespondFunction<T> = T extends undefined
-  ? (val?: ErrorReponse) => void
-  : (val: T | ErrorReponse) => void;
-export type EventWithAwk<T extends Record<string, unknown>, R = undefined> = (
-  args: T,
-  respond: RespondFunction<R>,
-) => void;
-
-export interface ServerToClientEvents {}
-export interface ClientToServerEvents {
-  "create-list": EventWithAwk<{ name: string }, List>;
-  "delete-list": EventWithAwk<{ id: string }>;
-  "join-list": EventWithAwk<{ id: string }>;
-  "get-list": EventWithAwk<{ id: string }, List>;
-  "add-item": EventWithAwk<{ listId: string; text: string }, Item>;
-}
