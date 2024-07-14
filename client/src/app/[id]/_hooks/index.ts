58570bd83478768f5daf7d9c76386d55e90a384a
turtledev <turtledev65@gmail.com>
2024-07-14 17:07:44 +0300

    [frontend] refactor: rename files using a lower-case naming convention

--- client/src/app/[id]/_hooks/index.ts
+++ client/src/app/[id]/_hooks/index.ts
@@ -1,4 +1,4 @@
-export { default as useGetList } from "./useGetList";
-export { default as useAddItem } from "./useAddItem";
-export { default as useDeleteItem } from "./useDeleteItem";
-export { default as useEditItem } from "./useEditItem";
+export { default as useGetList } from "./use-get-list";
+export { default as useAddItem } from "./use-add-item";
+export { default as useDeleteItem } from "./use-delete-item";
+export { default as useEditItem } from "./use-edit-item";
