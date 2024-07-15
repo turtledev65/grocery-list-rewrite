94c1a608335be247ab2e9b4c5436175040637806
turtledev <turtledev65@gmail.com>
2024-07-16 02:43:38 +0300

    [frontend] chore: create a new add item button that lets you add text items and items with images

--- client/src/app/list/[id]/page.tsx
+++ client/src/app/list/[id]/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import { Item, ItemForm } from "./_components";
-import { useGetList } from "./_hooks";
+import { ChangeEvent, FormEvent, useCallback, useRef, useState } from "react";
+import { Item } from "./_components";
+import { useAddItem, useGetList } from "./_hooks";
 
 type Props = {
   params: {
@@ -10,32 +11,102 @@ type Props = {
 };
 
 const ListPage = ({ params }: Props) => {
-  const { data: list, status, error } = useGetList(params.id);
+  const { data: list } = useGetList(params.id);
+
+  const textInputRef = useRef<HTMLInputElement>(null);
+  const { mutate: addItem } = useAddItem(params.id);
+
+  const [creating, setCreating] = useState(false);
+  const handleAddItem = useCallback(
+    (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      setCreating(false);
+
+      const text = textInputRef.current?.value.trim();
+      if (!text) return;
+
+      addItem({ text });
+    },
+    [addItem],
+  );
 
   return (
-    <>
-      <h1 className="text-3xl font-bold">
-        {status == "pending"
-          ? "Loading..."
-          : status == "error"
-            ? "Error"
-            : list.name}
-      </h1>
-      {status == "error" && <p>{error.message}</p>}
+    <main className="p-4">
+      <h1 className="text-4xl font-bold">{list?.name}</h1>
       <ul>
         {list?.items?.map(item => (
           <Item
             text={item.text}
+            images={item.images}
             pending={item.pending}
             id={item.id}
-            listId={params.id}
-            images={item.images}
+            listId={item.listId}
             key={item.id}
           />
         ))}
+        {creating && (
+          <form onSubmit={handleAddItem}>
+            <input
+              autoFocus
+              onBlur={() => setCreating(false)}
+              ref={textInputRef}
+              className="outline-none"
+            />
+          </form>
+        )}
       </ul>
-      <ItemForm listId={params.id} />
-    </>
+      <AddButton
+        onCreateItemWithImages={imageFiles => addItem({ imageFiles })}
+        onCreateTextItem={() => setCreating(true)}
+      />
+    </main>
+  );
+};
+
+const AddButton = ({
+  onCreateTextItem,
+  onCreateItemWithImages,
+}: {
+  onCreateTextItem: () => void;
+  onCreateItemWithImages: (files: File[]) => void;
+}) => {
+
+  const handleAttachImages = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const fileList = e.target.files;
+      if (!fileList) return;
+
+      const files: File[] = [];
+      for (let i = 0; i < fileList.length; i++) files.push(fileList[i]);
+      console.log(files);
+
+      onCreateItemWithImages(files);
+    },
+    [onCreateItemWithImages],
+  );
+
+  return (
+    <div className="group fixed bottom-5 right-9 flex select-none flex-col gap-2 rounded-3xl text-4xl text-white transition-colors hover:bg-purple-900">
+      <div className="grid h-0 w-12 place-items-center rounded-full bg-purple-600 outline-none transition-all group-hover:h-12">
+        <input
+          type="file"
+          id="images"
+          accept="image/*"
+          hidden
+          onChange={handleAttachImages}
+        />
+        <label htmlFor="images" className="invisible group-hover:visible">
+          C
+        </label>
+      </div>
+
+      <button
+        onClick={onCreateTextItem}
+        className="h-12 w-12 rounded-full bg-purple-600 outline-none"
+      >
+        +
+      </button>
+    </div>
   );
 };
 
