3619b05e85afc692b00ebc426420d4087116f235
turtledev <turtledev65@gmail.com>
2024-07-16 14:17:39 +0300

    [frontend] feat: delete items when they are swiped

--- client/src/app/list/[id]/_components/item.tsx
+++ client/src/app/list/[id]/_components/item.tsx
@@ -1,13 +1,20 @@
 "use client";
 
-import { FormEvent, useCallback, useRef, useState } from "react";
-import { useDeleteItem, useEditItem } from "../_hooks";
-
 import { Item as ItemProps, Image as ImageProps } from "@/types";
+import {
+  FormEvent,
+  PropsWithChildren,
+  useCallback,
+  useRef,
+  useState,
+} from "react";
+import { useDeleteItem, useEditItem } from "../_hooks";
+import { motion } from "framer-motion";
 
 const Item = ({ id, listId, text, pending, images }: ItemProps) => {
   const formRef = useRef<HTMLFormElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
+
   const [editing, setEditing] = useState(false);
 
   const { mutate: deleteItem } = useDeleteItem(listId);
@@ -19,66 +26,71 @@ const Item = ({ id, listId, text, pending, images }: ItemProps) => {
       const newText = inputRef.current?.value.trim();
       if (!newText || newText === text) return;
 
-      setEditing(false);
+      inputRef?.current?.blur();
       editItem({ newText, id });
     },
-    [editItem, id, text],
+    [editItem, text, id],
   );
+
   const handleDeleteItem = useCallback(() => {
     deleteItem(id);
   }, [deleteItem, id]);
 
   return (
-    <li
-      className={`${pending && "text-gray-500"} flex flex-row items-center justify-between`}
-    >
-      <div className="flex flex-col">
-        <div className="flex flex-row items-center">
-          {text && (
-            <div
-              onClick={() => setEditing(!pending && true)}
-              className="w-full"
-            >
-              {editing ? (
-                <form ref={formRef} onSubmit={handleEditItem}>
-                  <input
-                    defaultValue={text}
-                    autoFocus
-                    ref={inputRef}
-                    onBlur={() => setEditing(false)}
-                    className="w-full"
-                  />
-                </form>
-              ) : (
-                <p>{text}</p>
-              )}
-            </div>
-          )}
-        </div>
-        <div className="flex flex-row gap-2">
-          {images?.map(img => (
-            <Image
-              id={img.id}
-              itemId={img.itemId}
-              url={img.url}
-              pending={img.pending}
-              key={img.id}
-            />
-          ))}
-        </div>
-      </div>
-      {!pending && !editing && (
-        <button onClick={handleDeleteItem} className="font-bold text-red-600">
-          X
-        </button>
-      )}
+    <li>
+      <SwipeableContainer
+        draggable={!editing && !pending}
+        onSwipe={handleDeleteItem}
+      >
+        <form ref={formRef} onSubmit={handleEditItem}>
+          <input
+            defaultValue={text}
+            ref={inputRef}
+            onFocus={() => setEditing(true)}
+            onBlur={() => {
+              setEditing(false);
+              formRef?.current?.reset();
+            }}
+            className={`${pending ? "text-gray-500" : "text-black"} w-full`}
+          />
+        </form>
+      </SwipeableContainer>
     </li>
+  );
+};
+
+type SwipeableContainerProps = {
+  draggable: boolean;
+  onSwipe: () => void;
+} & PropsWithChildren;
+const SwipeableContainer = ({
+  draggable,
+  onSwipe,
+  children,
+}: SwipeableContainerProps) => {
+  const DRAG_THRESHOLD = 100;
+
+  return (
+    <div className="relative">
+      <div className="absolute inset-0 z-[-10] bg-red-500">X</div>
+      <motion.div
+        drag={draggable && "x"}
+        dragConstraints={{ left: 0, right: 0 }}
+        dragElastic={{ left: 0.3 }}
+        onDragEnd={(_, info) => {
+          if (Math.abs(info.offset.x) >= DRAG_THRESHOLD) onSwipe();
+        }}
+        className="bg-gray-50"
+      >
+        {children}
+      </motion.div>
+    </div>
   );
 };
 
 const Image = ({ url, pending }: ImageProps) => {
   return (
-    <div className="relative">
+    <div className="w-full">
       <img src={url} className={`${pending && "grayscale filter"} max-w-sm`} />
       {pending && (
         <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
