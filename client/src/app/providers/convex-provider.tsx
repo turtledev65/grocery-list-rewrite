6865fdb91cfc20e99d91ff05b321c55b80f1e37c
turtledev <turtledev65@gmail.com>
2024-07-28 17:55:02 +0300

    [frontend] chore: set up react query with convex

--- client/src/app/providers/convex-provider.tsx
+++ client/src/app/providers/convex-provider.tsx
@@ -1,11 +1,45 @@
 "use client";
 
+import { ConvexQueryClient } from "@convex-dev/react-query";
+import {
+  isServer,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { PropsWithChildren } from "react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
-const ConvexClientProvider = ({ children }: PropsWithChildren) => {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
-};
+let browserQueryClient: QueryClient | undefined = undefined;
 
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convexQueryClient = new ConvexQueryClient(convex);
+const queryClient = getQueryClient();
+convexQueryClient.connect(queryClient);
+
+const ConvexClientProvider = ({ children }: PropsWithChildren) => {
+  return (
+    <ConvexProvider client={convex}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </ConvexProvider>
+  );
+};
 export default ConvexClientProvider;
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        queryKeyHashFn: convexQueryClient.hashFn(),
+        queryFn: convexQueryClient.queryFn(),
+      },
+    },
+  });
+}
+function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+}
--- client/src/app/providers/index.tsx
+++ client/src/app/providers/index.tsx
@@ -1,56 +1,22 @@
 "use client";
 
 import { PropsWithChildren } from "react";
-import {
-  isServer,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
 import PanelProvider from "./panel-provider";
 import SidebarProvider from "./sidebar-provider";
 import LastOpeendListProvider from "./last-opened-list-provider";
 import SettingsProvider from "./settings-provider";
 import ConvexClientProvider from "./convex-provider";
 
-function makeQueryClient() {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: Infinity,
-        retry: 0,
-        refetchOnMount: false,
-        refetchOnReconnect: false,
-        refetchOnWindowFocus: false,
-      },
-    },
-  });
-}
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-function getQueryClient() {
-  if (isServer) {
-    return makeQueryClient();
-  } else {
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-}
-
 const Providers = ({ children }: PropsWithChildren) => {
-  const queryClient = getQueryClient();
-
   return (
     <ConvexClientProvider>
-      <QueryClientProvider client={queryClient}>
-        <SettingsProvider>
-          <PanelProvider>
-            <SidebarProvider>
-              <LastOpeendListProvider>{children}</LastOpeendListProvider>
-            </SidebarProvider>
-          </PanelProvider>
-        </SettingsProvider>
-      </QueryClientProvider>
+      <SettingsProvider>
+        <PanelProvider>
+          <SidebarProvider>
+            <LastOpeendListProvider>{children}</LastOpeendListProvider>
+          </SidebarProvider>
+        </PanelProvider>
+      </SettingsProvider>
     </ConvexClientProvider>
   );
 };
