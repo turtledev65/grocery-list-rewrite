dc17801e72a173bc98ce6361167a39fe843cbc3b
turtledev <turtledev65@gmail.com>
2024-07-31 19:41:20 +0300

    [frontend] fix: use h-svh instead of h-screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} h-screen`}>
+      <body className={`${inter.className} h-svh`}>
         <Providers>
           <Sidebar />
           <Panel />
