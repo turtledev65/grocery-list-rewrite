74cffb0c99f4b825ee5a177d0add4891676e21b1
turtledev <turtledev65@gmail.com>
2024-07-31 23:07:56 +0300

    [frontend] feat: display a loading spinner if the status is pending and display an error if the status is error

--- src/app/list/[id]/page.tsx
+++ src/app/list/[id]/page.tsx
@@ -10,6 +10,9 @@ import { Id } from "../../../../convex/_generated/dataModel";
 import { api } from "../../../../convex/_generated/api";
 import NewItemForm from "./_components/new-item-form";
 import NewItemProvider from "./_components/new-item-provider";
+import { useEffect } from "react";
+import LoadingSpinner from "@/app/components/loading-spinner";
+import Link from "next/link";
 
 type Props = {
   params: {
@@ -21,14 +24,43 @@ const ListPage = ({ params }: Props) => {
   const searchParams = useSearchParams();
   const isNew = searchParams.get("new")?.toLowerCase() === "true";
 
-  const { data: list } = useQuery(
+  const {
+    data: list,
+    status,
+    error,
+  } = useQuery(
     convexQuery(api.list.getList, {
       id: params.id as Id<"lists">,
     }),
   );
 
+  if (status === "pending")
+    return (
+      <div className="grid h-full place-items-center">
+        <LoadingSpinner />
+      </div>
+    );
+
+  if (status === "error")
+    return (
+      <div className="p-4">
+        <h1 className="mb-8 text-4xl font-bold">
+          An Errror occured
+        </h1>
+        <p className="text-left text-red-600">{error.message}</p>
+        <div className="absolute bottom-0 left-0 flex w-full items-center justify-center py-8">
+          <Link
+            href="/home"
+            className="select-none rounded-md bg-purple-600 px-6 py-2 text-lg text-gray-50 active:opacity-80"
+          >
+            Go Home
+          </Link>
+        </div>
+      </div>
+    );
+
   return (
-    <main className="overflow-y-auto overflow-x-hidden h-full pb-6">
+    <main className="h-full overflow-y-auto overflow-x-hidden pb-6">
       <ListTitle
         title={list?.name ?? ""}
         listId={list?._id ?? ""}
