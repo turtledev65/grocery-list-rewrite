634a7e5bdacd68a26d34e90793855a55751a243f
turtledev <turtledev65@gmail.com>
2024-07-31 22:07:05 +0300

    [frontend] fix: rewrite the local storage hook to fix the keys being reset when refreshing

--- src/app/hooks/util/useLocalStorage.ts
+++ src/app/hooks/util/useLocalStorage.ts
@@ -1,34 +1,45 @@
 "use client";
 
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 
 export default function useLocalStorage<T>(
   key: string,
   defaultValue?: T,
 ): [T | undefined, Dispatch<SetStateAction<T | undefined>>] {
-  const [state, setState] = useState<T | undefined>();
+  const isMounted = useRef(false);
+  const [state, setState] = useState<T | undefined>(defaultValue);
 
   useEffect(() => {
-    let val: T | undefined;
-
-    const jsonVal = localStorage.getItem(key);
-    if (jsonVal !== null) val = JSON.parse(jsonVal);
-    else val = defaultValue;
-
-    setState(val);
-  }, []);
-
-  useEffect(() => {
-    if (state === undefined) return localStorage.removeItem(key);
-
-    let res: string;
     try {
-      res = JSON.stringify(state);
+      const item = localStorage.getItem(key);
+      if (item) setState(JSON.parse(item));
     } catch (err) {
-      res = "";
+      console.error(err);
     }
-    localStorage.setItem(key, res);
-  }, [state]);
+    return () => {
+      isMounted.current = false;
+    };
+  }, [key]);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      if (state === undefined) {
+        localStorage.removeItem(key);
+        return;
+      }
+
+      let res: string | undefined = undefined;
+      try {
+        res = JSON.stringify(state);
+      } catch (err) {
+        console.error(err);
+      }
+
+      if (typeof res === "string") localStorage.setItem(key, res);
+    } else {
+      isMounted.current = true;
+    }
+  }, [state, key]);
 
   return [state, setState];
 }
