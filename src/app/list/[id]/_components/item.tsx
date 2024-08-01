a2a69330f2086f9c4e97b4d90dadc838f0798667
turtledev <turtledev65@gmail.com>
2024-08-01 13:42:26 +0300

    [frontend] style: make the external icon purple

--- src/app/list/[id]/_components/item.tsx
+++ src/app/list/[id]/_components/item.tsx
@@ -160,7 +160,7 @@ const ZoomableImage = ({ src, name }: { src: string; name: string }) => {
                     className="flex flex-row items-center gap-4"
                   >
                     {name}
-                    <ExternalIcon />
+                    <ExternalIcon className="text-purple-500" />
                   </motion.span>
                 </Link>
                 <motion.div
