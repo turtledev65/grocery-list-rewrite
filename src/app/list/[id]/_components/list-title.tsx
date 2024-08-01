9ba43cae72a9c8f5108594c41674bc64c2eb8116
turtledev <turtledev65@gmail.com>
2024-08-01 12:43:17 +0300

    [frontend] chore: remove the isNew search params after renaming the list

--- src/app/list/[id]/_components/list-title.tsx
+++ src/app/list/[id]/_components/list-title.tsx
@@ -1,35 +1,38 @@
 "use client";
 
-import { useMutation } from "convex/react";
+import { useMutation } from "@tanstack/react-query";
 import { FormEvent, useCallback, useEffect, useRef } from "react";
 import { api } from "../../../../../convex/_generated/api";
 import { Id } from "../../../../../convex/_generated/dataModel";
+import { useConvexMutation } from "@convex-dev/react-query";
+import { useRouter } from "next/navigation";
 
-const ListTitle = ({
-  title,
-  listId,
-  isNew,
-}: {
+type Props = {
   title: string;
   listId: string;
   isNew: boolean;
-}) => {
+};
+const ListTitle = ({ title, listId, isNew }: Props) => {
   const formRef = useRef<HTMLFormElement>(null);
   const inputRef = useRef<HTMLInputElement>(null);
 
-  const renameList = useMutation(api.list.renameList).withOptimisticUpdate(
-    (localStore, args) => {
-      const { newName, id } = args;
-      const currVal = localStore.getQuery(api.list.getList, { id });
-      if (!currVal) return;
+  const router = useRouter();
 
-      localStore.setQuery(
-        api.list.getList,
-        { id },
-        { ...currVal, name: newName },
-      );
-    },
-  );
+  const { mutate: renameList } = useMutation({
+    mutationFn: useConvexMutation(api.list.renameList).withOptimisticUpdate(
+      (localStore, args) => {
+        const { newName, id } = args;
+        const currVal = localStore.getQuery(api.list.getList, { id });
+        if (!currVal) return;
+
+        localStore.setQuery(
+          api.list.getList,
+          { id },
+          { ...currVal, name: newName },
+        );
+      },
+    ),
+  });
   const handleRenameList = useCallback(
     (e: FormEvent<HTMLFormElement>) => {
       e.preventDefault();
@@ -37,24 +40,25 @@ const ListTitle = ({
       if (!newName || newName === title) return;
 
       inputRef.current?.blur();
-      renameList({ id: listId as Id<"lists">, newName });
+      renameList(
+        { id: listId as Id<"lists">, newName },
+        { onSuccess: () => router.push(`/list/${listId}`) },
+      );
     },
-    [title, renameList, listId],
+    [title, renameList, listId, router],
   );
 
   useEffect(() => {
     if (!inputRef.current) return;
-    if (!isNew) return;
-
-    inputRef.current.focus();
-    inputRef.current.select();
-  }, [isNew, title]);
+    if (isNew) inputRef.current.select();
+  }, [isNew]);
 
   return (
     <form onSubmit={handleRenameList} ref={formRef}>
       <input
         type="text"
         defaultValue={title}
+        autoFocus={isNew}
         ref={inputRef}
         onBlur={() => formRef.current?.reset()}
         className="w-full bg-gray-50 px-2 pb-4 text-4xl font-bold outline-none dark:bg-zinc-900"
