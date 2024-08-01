3925a475d5a7153c9822f07312bf4d356d241b83
turtledev <turtledev65@gmail.com>
2024-08-01 13:11:54 +0300

    [frontend] style: add a logo

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,15 +1,19 @@
+import Image from "next/image";
+
 const AboutPage = () => {
   return (
     <div className="flex h-full items-center justify-center">
-      <div className="bg-gray-400 p-20 text-white motion-safe:animate-float">
-        Logo
-      </div>
+      <Image
+        src="logo.svg"
+        width={128}
+        height={128}
+        alt="logo"
+        className="w-72 motion-safe:animate-float"
+      />
       <div className="absolute bottom-0 w-full pb-6">
         <h1 className="text-center text-4xl font-bold">
           Grocery List
-          <span className="text-base text-gray-400 ml-2">
-            2.0
-          </span>
+          <span className="ml-2 text-base text-gray-400">2.0</span>
         </h1>
       </div>
     </div>
